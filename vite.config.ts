b5f53ac3b577aad08709f71e17b53d95a62c282e
Artem Netsvetaev <netsvour@gmail.com>
2026-08-27 13:59:06 +0300

    обновлен react

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import react from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
 // import mkcert from "vite-plugin-mkcert";
@@ -10,6 +11,9 @@ export default defineConfig({
 	},
 	plugins: [
 		react(),
+		babel({
+			presets: [reactCompilerPreset()],
+		}),
 		// mkcert()
 	],
 	resolve: {
