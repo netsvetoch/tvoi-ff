7f8bff1c55937b0a5bbb56019a531b8c9fd2fbf0
Artem Netsvetaev <netsvour@gmail.com>
2026-08-27 12:44:30 +0300

    добавил repos в исключения

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,7 +11,7 @@ import { configs as tseslint } from "typescript-eslint";
 
 export default defineConfig([
 	{
-		ignores: ["**/*.d.ts", "dist", "node_modules", "src/shared/api"],
+		ignores: ["**/*.d.ts", "dist", "node_modules", "src/shared/api", "repos"],
 	},
 	{ files: ["**/*.{js,mjs,cjs,ts}"] },
 	{
